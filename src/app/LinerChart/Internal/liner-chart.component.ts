8d31d129f3a4948cb8ba29082624a70ed15273b8
Ali Dehghani <alixdehghani@gmail.com>
2022-04-18 23:54:15 +0430

    refactor (line chart + live) component: add toolbox and improve performance

--- src/app/LinerChart/Internal/liner-chart.component.ts
+++ src/app/LinerChart/Internal/liner-chart.component.ts
@@ -1,119 +1,107 @@
 import { Component, OnInit } from '@angular/core';
-import { LineChart } from 'echarts/charts';
-import { TitleComponent, DataZoomComponent, ToolboxComponent, TooltipComponent, GridComponent, LegendComponent } from 'echarts/components';
+import { EChartsOption } from 'echarts';
+import { BarChart, LineChart } from 'echarts/charts';
+import * as echarts from 'echarts/core';
+import {
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+} from 'echarts/components';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-liner-chart',
   templateUrl: './liner-chart.component.html',
-  styleUrls: ['./liner-chart.component.scss']
+  styleUrls: ['./liner-chart.component.scss'],
 })
 export class LinerChartComponent implements OnInit {
-  data: any[] = [];
-  now: any = +new Date(1997, 9, 3);
-  oneDay = 24 * 3600 * 1000;
-  value = Math.random() * 1000;
-  echartsExtentions: any[];
-  private _randomData() {
-    this.now = new Date(+this.now + this.oneDay);
-    this.value = this.value + Math.random() * 21 - 10;
-    return {
-      name: this.now.toString(),
-      value: [
-        [this.now.getFullYear(), this.now.getMonth() + 1, this.now.getDate()].join('/'),
-        Math.round(this.value)
-      ]
-    };
+  chartData: IChartData[] = [];
+  readonly echartsExtentions: any[];
+  option: EChartsOption;
+  chartInstance: echarts.ECharts;
+  constructor() {
+    this.echartsExtentions = [
+      TitleComponent,
+      LineChart,
+      BarChart,
+      ToolboxComponent,
+      TooltipComponent,
+      GridComponent,
+      LegendComponent,
+    ];
   }
-  option = {
-    title: {
-      text: 'Liner chart + live'
-    },
-    tooltip: {
-      trigger: 'axis',
-      formatter: function (params: any) {
-        params = params[0];
-        var date = new Date(params.name);
-        return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-      },
-      axisPointer: {
-        animation: false
-      }
-    },
-    xAxis: {
-      type: 'time',
-      splitLine: {
-        show: false
-      }
-    },
-    yAxis: {
-      type: 'value',
-      boundaryGap: [0, '100%'],
-      splitLine: {
-        show: false
-      }
-    },
-    series: [{
-      name: 'msft',
-      type: 'line',
-      showSymbol: false,
-      hoverAnimation: false,
-      data: this.data
-    }]
-  };
-  constructor () {
-    this.echartsExtentions = [ TitleComponent ,DataZoomComponent,LineChart,ToolboxComponent,TooltipComponent, GridComponent, LegendComponent];
-   }
-
+  
   ngOnInit(): void {
-    for (var i = 0; i < 1000; i++) {
-      this.data.push(this._randomData());
-    }
-    setInterval(() => {
-
-      for (var i = 0; i < 5; i++) {
-        this.data.shift();
-        this.data.push(this._randomData());
-      }
-
-      this._restOptions(this.data);
-    }, 1000);
-  }
-  private _restOptions(data: any[]) {
     this.option = {
       title: {
-        text: '动态数据 + 时间坐标轴'
+        text: 'Liner chart + live',
       },
       tooltip: {
         trigger: 'axis',
-        formatter: function (params: any) {
-          params = params[0];
-          var date = new Date(params.name);
-          return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
-        },
         axisPointer: {
-          animation: false
-        }
+          animation: false,
+        },
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          dataView: { readOnly: true },
+          magicType: { type: ['line', 'bar'] },
+          saveAsImage: { name: 'Line chart' },
+        },
       },
       xAxis: {
-        type: 'time',
+        type: 'category',
         splitLine: {
-          show: false
-        }
+          show: true,
+        },
       },
       yAxis: {
         type: 'value',
-        boundaryGap: [0, '100%'],
         splitLine: {
-          show: false
-        }
+          show: false,
+        },
       },
-      series: [{
-        name: '模拟数据',
-        type: 'line',
-        showSymbol: false,
-        hoverAnimation: false,
-        data: data
-      }]
+      series: [
+        {
+          type: 'line',
+          data: this.chartData,
+        },
+      ],
+    };
+
+    interval(1000).subscribe({
+      next: () => {
+        const data = this._generateRandomData();
+        this.chartData.push(data);
+        this.chartInstance.setOption({
+          series: [
+            {
+              data: this.chartData,
+            },
+          ],
+        });
+      },
+    });
+  }
+
+  private _generateRandomData(): IChartData {
+    return {
+      name: new Date().toString(),
+      value: [
+        [
+          new Date().getHours(),
+          new Date().getMinutes(),
+          new Date().getSeconds(),
+        ].join(':'),
+        Math.round(Math.random() * 1000),
+      ],
     };
   }
+}
+interface IChartData {
+  name: string;
+  value: [string, number];
 }
